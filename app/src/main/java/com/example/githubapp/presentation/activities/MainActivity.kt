7073a8aa1846add1577ba8c82238dd0b2c59012d
Designeed <v.burczev2013@yandex.ru>
2022-06-17 21:06:06 +0700

    added auth screen

--- app/src/main/java/com/example/githubapp/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/presentation/activities/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.githubapp.presentation.activities
 
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.githubapp.R
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_auth)
     }
 }
