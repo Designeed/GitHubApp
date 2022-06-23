dc0016f00e88380094edeb51b55454402ca7b2bb
Designeed <v.burczev2013@yandex.ru>
2022-06-23 18:54:44 +0700

    added authorization module

--- app/src/main/java/com/example/githubapp/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/presentation/activities/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.githubapp.presentation.activities
 
 import android.os.Bundle
-import android.view.WindowManager
-import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.githubapp.R
-import com.google.android.material.textfield.TextInputLayout
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupNavController()
+    }
+
+    private fun setupNavController() {
+        navController = (supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment).navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
