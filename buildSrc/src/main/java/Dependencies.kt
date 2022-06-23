3cff322d2cdd6358fb540df0e1e6927267c149d1
Designeed <v.burczev2013@yandex.ru>
2022-06-23 18:57:40 +0700

    change version for kotlin and hilt

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,7 +1,7 @@
 object Dependencies {
 
     object Dagger {
-        private const val version = "2.40.5"
+        private const val version = "2.42"
 
         const val hilt = "com.google.dagger:hilt-android:$version"
         const val hiltCompiler = "com.google.dagger:hilt-android-compiler:$version"
